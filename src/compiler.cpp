58b5a02ac84b6bdd84ca7a651e766153acfd3b2d
jsnal <jasonlongball@gmail.com>
2019-04-28 20:16:55 -0400

    push built files without metadata and astyle

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <sstream>
+#include <sys/stat.h>
 #include <algorithm>
 #include <fstream>
 #include <dirent.h>
 #include <vector>
+#include <iomanip>
 #include "compiler.h"
 
 int get_file_list(std::string dir, std::vector<std::string> &files)
 {
     DIR *dp;
     struct dirent *dirp;
-    if((dp  = opendir(dir.c_str())) == NULL) {
+    if((dp  = opendir(dir.c_str())) == NULL)
+    {
         std::cout << "Error(" << errno << ") opening " << dir << std::endl;
         return errno;
     }
 
-    while ((dirp = readdir(dp)) != NULL) {
+    while ((dirp = readdir(dp)) != NULL)
+    {
         files.push_back(std::string(dirp->d_name));
     }
     files.erase(std::remove(files.begin(), files.end(), "."), files.end());
@@ -34,28 +38,54 @@ std::string get_file(std::string file)
 
 std::string compile_get_id(std::string file, std::string id)
 {
-		std::istringstream f(get_file(file));
-		std::string cline;
-		for (unsigned int i = 0; i <= 2; i++) {
-				std::getline(f, cline);
-				if (cline.find(id) != std::string::npos) {
-					break;
-				}
-		}
-	 return	cline.erase(0, id.length());
+    std::istringstream f(get_file(file));
+    std::string cline;
+    for (unsigned int i = 0; i <= 2; i++)
+    {
+        std::getline(f, cline);
+        if (cline.find(id) != std::string::npos)
+        {
+            break;
+        }
+    }
+    return	cline.erase(0, id.length());
+}
+
+void format_file(const std::string& file)
+{
+    mkdir("build", 0777);
+    std::istringstream f(get_file(file));
+    std::string cline;
+    std::ofstream outfile ("build/" + file + ".paste");
+    int i;
+    while(std::getline(f, cline))
+    {
+        i++;
+        if (i > 3)
+        {
+            outfile << cline << std::endl;
+        }
+    }
+    outfile.close();
 }
 
 void compile_table_header(std::ostream& os)
 {
-		os << "<html>\n<header>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n<script charset='utf-8' src='js/index.js'></script>\n</header>\n"
-			 << "<table id=\"paste-tbl-container\">\n<tr class=\"paste-tbl-row\">\n"
-			 << "<th class=\"paste-tbl-header\">Name</th>\n<th class=\"paste-tbl-header\">Date</th>\n<th class=\"paste-tbl-header\">Filetype</th>\n<th class=\"paste-tbl-header\">View</th>\n</tr>"
-			 << std::endl;
+    os << "<html>\n<header>\n<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">\n<script charset='utf-8' src='js/index.js'></script>\n</header>\n"
+       << "<table id=\"paste-tbl-container\">\n<tr class=\"paste-tbl-row\">\n"
+       << "<th class=\"paste-tbl-header\">Name</th>\n<th class=\"paste-tbl-header\">Date</th>\n<th class=\"paste-tbl-header\">Filetype</th>\n<th class=\"paste-tbl-header\">View</th>\n</tr>"
+       << std::endl;
 }
 
 void compile_table_row(std::ostream& os, const std::string& value)
 {
-		os << "<td class=\"paste-tbl-data\">" << value << "</td>" << std::endl;
+    os << "<td class=\"paste-tbl-data\">" << value << "</td>" << std::endl;
+}
+
+void compile_table_footer(std::ostream& os)
+{
+    auto time = std::time(nullptr);
+    os << "<footer>Last Updated: " << std::put_time(std::gmtime(&time), "%D") << "</footer>" << std::endl;
 }
 
 int paste_compile()
@@ -63,17 +93,22 @@ int paste_compile()
     std::vector<std::string> files = std::vector<std::string>();
     get_file_list("posts", files);
 
-		std::ofstream outfile ("index.html");
+    std::ofstream outfile ("index.html");
 
-		compile_table_header(outfile);
+    compile_table_header(outfile);
 
-    for (unsigned int i = 0;i < files.size();i++) {
-				outfile << "<tr class=\"paste-tbl-row\">" << std::endl;
-				compile_table_row(outfile, compile_get_id(files[i], "//*title="));
-				compile_table_row(outfile, compile_get_id(files[i], "//*date="));
-				compile_table_row(outfile, compile_get_id(files[i], "//*filetype="));
-				outfile << "</tr>" << std::endl;
-		}
-		outfile.close();
+    for (unsigned int i = 0; i < files.size(); i++)
+    {
+        format_file(files[i]);
+        outfile << "<tr class=\"paste-tbl-row\">" << std::endl;
+        compile_table_row(outfile, compile_get_id(files[i], "//*title="));
+        compile_table_row(outfile, compile_get_id(files[i], "//*date="));
+        compile_table_row(outfile, compile_get_id(files[i], "//*filetype="));
+        compile_table_row(outfile, "<a href=\"build/" + files[i] + ".paste\">GET</a>");
+        outfile << "</tr>" << std::endl;
+    }
+    outfile << "</table>" << std::endl;
+    compile_table_footer(outfile);
+    outfile.close();
     return 0;
 }

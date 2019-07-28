5be6970a3f747e7cb1e3928c28e19a55577e97b4
jsnal <jasonlongball@gmail.com>
2019-07-28 16:34:33 -0400

    catching junk going into the nc port

--- server/paste-listener.sh
+++ server/paste-listener.sh
@@ -3,8 +3,12 @@
 
 SEND_OUTPUT_PORT="5555";
 
-while true ; do
+while true; do
   paste_input=$(nc -l -q 0 -p $SEND_OUTPUT_PORT)
+  if ! echo $paste_input | sed 1q | grep '//\**' > /dev/null; then
+    echo "File found but not a valid paste... Skipping"
+    continue;
+  fi
   filename=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
   echo "$paste_input" > posts/"$filename".txt
   paste --compile

0c5935721002456f0e21dff03c0d055b6c4e0def
jsnal <jasonlongball@gmail.com>
2019-12-18 10:11:14 -0500

    feat: Move the file functions to a utils directory
    Cleaning up the format of the code be taking out some reusable code and
    putting it in a utils directory.

---
+++ include/project/utils/file.hpp
@@ -0,0 +1,4 @@
+using namespace std;
+
+extern int get_file_list(string, vector<string>&);
+extern string get_file(string);
--- src/compiler.cpp
+++ src/compiler.cpp
@@ -12,39 +12,10 @@
 #include "project/html_generator.hpp"
 #include "project/file_format.hpp"
 #include "project/paste_config.hpp"
+#include "project/utils/file.hpp"
 #include "maddy/parser.h"
 #include "ctml.hpp"
 
-
-int get_file_list(std::string dir, std::vector<std::string> &files)
-{
-    DIR *dp;
-    struct dirent *dirp;
-
-    if((dp  = opendir(dir.c_str())) == NULL)
-    {
-        std::cout << "Error(" << errno << ") opening " << dir << std::endl;
-        return errno;
-    }
-
-    while ((dirp = readdir(dp)) != NULL)
-    {
-        files.push_back(std::string(dirp->d_name));
-    }
-
-    files.erase(std::remove(files.begin(), files.end(), "."), files.end());
-    files.erase(std::remove(files.begin(), files.end(), ".."), files.end());
-    closedir(dp);
-    return 0;
-}
-
-std::string get_file(std::string file)
-{
-    std::ifstream ifs("posts/" + file);
-    std::string content( (std::istreambuf_iterator<char>(ifs) ),
-                         (std::istreambuf_iterator<char>()    ) );
-    return content;
-}
 
 std::string compile_get_id(std::string file, std::string id)
 {
